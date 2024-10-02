23446b7d3ce6e684ed7ceb06f4ba8ae5cbd16d27
IT-Schnitzel <oficealex@mail.ru>
2024-10-02 23:12:14 +0300

    Added tests ect + poms

---
+++ kudago/src/test/java/com/example/kudago/KudagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kudago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KudagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ newCrudRest/src/test/java/task/newCrudRest/NewCrudRestApplicationTests.java
@@ -0,0 +1,13 @@
+package task.newCrudRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewCrudRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
